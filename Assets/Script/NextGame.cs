a60aa582efc43a0fbff9d03500112d30d51eb3a6
YungKwanWai <161811467+SnowYung@users.noreply.github.com>
2024-04-22 23:51:34 +0800

    Update Unity

--- Assets/Script/NextGame.cs
+++ Assets/Script/NextGame.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class NextGame : MonoBehaviour
 {
+    public FirstPersonController fpsController;
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync("Gaming");
     }
     public void UI()
     {
+        fpsController.SetCursorVisibility(false);
         SceneManager.LoadSceneAsync("UI");
     }
     public void Quit()
